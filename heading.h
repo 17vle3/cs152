85278531c179e10d1316a6557d2c4d05831b433e
17vle3 <17vle3@gmail.com>
2020-06-07 20:24:33 -0700

    Update heading.h

--- heading.h
+++ heading.h
@@ -8,3 +8,19 @@
 #include <stdio.h>
 #include <string>
 using namespace std;
+
+struct Terminal{
+   stringstream *code;
+   string *place;
+   string *value;
+   string *offset;
+   string *op;
+   string *begin;
+   string *parent;
+   string *end;
+   Type type;
+   int length;
+   string *index;
+   vector<string> *ids;
+   vector<Var> *vars; 
+};
