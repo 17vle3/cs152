56cd755f8a07dbb5f270341953e1cdbeba1fc3aa
17vle3 <17vle3@gmail.com>
2020-06-07 20:25:07 -0700

    Update heading.h

--- heading.h
+++ heading.h
@@ -1,26 +1,64 @@
+/* heading.h */
+#ifndef _heading_h_
+#define _heading_h_
+
 #define YY_NO_UNPUT
+
 #include <iostream>
+
+/*data structures*/
 #include <vector>
 #include <stack>
 #include <map>
+
+
 #include <sstream>
 #include <fstream>
 #include <stdio.h>
 #include <string>
+
 using namespace std;
 
-struct Terminal{
-   stringstream *code;
-   string *place;
-   string *value;
-   string *offset;
-   string *op;
-   string *begin;
-   string *parent;
-   string *end;
-   Type type;
-   int length;
-   string *index;
-   vector<string> *ids;
-   vector<Var> *vars; 
-};
+enum Type {INT,INT_ARR,FUNC};
+
+    struct Var{
+        
+        string *place;
+        string *value;
+        string *offset;
+        //vector
+        Type type;
+        int length;
+        string *index;
+    } ;
+
+    struct Loop{
+        string *begin;
+        string *parent;
+        string *end;
+    };
+
+
+    struct Terminal{
+       stringstream *code;
+       //location
+       string *place;
+       string *value;
+       string *offset;
+       // branches
+       string *op;
+       string *begin;
+       string *parent;
+       string *end;
+       // type
+       //uint val;
+       Type type;
+       int length;
+       string *index;
+       // idents and vars
+       vector<string> *ids;
+       vector<Var> *vars; 
+    };
+
+
+#endif
